32fb46d0a4695d0fb32b1f0ef19851766626fe7a
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-03-06 15:08:44 -0500

    Add some tests for SM field reader

--- client/src/formats/sm/fieldReader.spec.ts
+++ client/src/formats/sm/fieldReader.spec.ts
@@ -1,6 +1,40 @@
+import assert from "assert";
+import { readFields } from "./fieldReader";
+
 describe("sm/fieldReader", () => {
     describe("#readFields", () => {
-        it("reads an empty field");
-        it("ignores comments");
+        it("returns an empty array if the content is empty", () => {
+            assert(readFields("").length === 0);
+        });
+
+        it("reads an empty field", () => {
+            const fields = readFields("#KEY:;");
+            assert.deepStrictEqual(fields, [{ name: "KEY", value: "" }]);
+        });
+
+        it("reads multiple fields", () => {
+            const fields = readFields("#FIELD1:a;\n#FIELD2:b;");
+            assert.deepStrictEqual(fields, [
+                { name: "FIELD1", value: "a" },
+                { name: "FIELD2", value: "b" },
+            ]);
+        });
+
+        it("reads multiline fields and converts CRLF to LF", () => {
+            const fields = readFields("#FIELD1:abc\r\ndef;#FIELD2:foo\nbar;");
+            assert.deepStrictEqual(fields, [
+                { name: "FIELD1", value: "abc\ndef" },
+                { name: "FIELD2", value: "foo\nbar" },
+            ]);
+        });
+
+        it("ignores comments", () => {
+            const fields = readFields("//#FIELD1:a;\n#FIELD2:b;");
+            assert.deepStrictEqual(fields, [{ name: "FIELD2", value: "b" }]);
+        });
+
+        it("throws an error if it hits EOF while reading", () => {
+            assert.throws(() => readFields("#KEY:value"));
+        });
     });
 });
