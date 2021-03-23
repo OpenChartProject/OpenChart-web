232e8367d76e1ad2203bb71935c10086e4094605
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-03-22 20:14:10 -0400

    Add some common beat snappings

--- client/src/notefield/beatsnap.ts
+++ client/src/notefield/beatsnap.ts
@@ -2,6 +2,19 @@ import { Beat } from "../charting/beat";
 import Fraction from "fraction.js";
 import assert from "assert";
 
+export const commonBeatSnaps: Readonly<Fraction>[] = [
+    new Fraction(1, 4),
+    new Fraction(1, 8),
+    new Fraction(1, 12),
+    new Fraction(1, 16),
+    new Fraction(1, 24),
+    new Fraction(1, 32),
+    new Fraction(1, 48),
+    new Fraction(1, 64),
+    new Fraction(1, 96),
+    new Fraction(1, 192),
+];
+
 export class BeatSnap {
     private _current!: Fraction;
 
