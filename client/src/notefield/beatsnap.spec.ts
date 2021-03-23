a1afc13776ec7e084afb72f99d43612d94f75a95
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-03-22 20:49:49 -0400

    Add more beat snap methods/tests

--- client/src/notefield/beatsnap.spec.ts
+++ client/src/notefield/beatsnap.spec.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import Fraction from "fraction.js";
 import { Beat } from "../charting/beat";
-import { BeatSnap } from "./beatsnap";
+import { BeatSnap, commonBeatSnaps } from "./beatsnap";
 
 describe("BeatSnap", () => {
     describe("new", () => {
@@ -14,6 +14,88 @@ describe("BeatSnap", () => {
         it("throws if the snap is lte zero", () => {
             assert.throws(() => new BeatSnap(new Fraction(0)));
             assert.throws(() => new BeatSnap(new Fraction(-1)));
+        });
+    });
+
+    describe("#isCommonSnap", () => {
+        it("returns true for common beat snappings", () => {
+            for (const snap of commonBeatSnaps) {
+                assert(new BeatSnap(snap).isCommonSnap());
+            }
+        });
+
+        it("returns false if beat snapping is not common", () => {
+            assert(!new BeatSnap(new Fraction(1, 6)).isCommonSnap());
+            assert(!new BeatSnap(new Fraction(3, 8)).isCommonSnap());
+        });
+    });
+
+    describe("#nearestCommonSnapIndex", () => {
+        it("returns expected index if snapping is common", () => {
+            for (let i = 0; i < commonBeatSnaps.length; i++) {
+                const snap = commonBeatSnaps[i];
+                assert.strictEqual(
+                    new BeatSnap(snap).nearestCommonSnapIndex(),
+                    i,
+                );
+            }
+        });
+
+        it("returns nearest common snapping", () => {
+            assert.strictEqual(
+                new BeatSnap(new Fraction(1, 3)).nearestCommonSnapIndex(),
+                0,
+            );
+            assert.strictEqual(
+                new BeatSnap(new Fraction(1, 6)).nearestCommonSnapIndex(),
+                1,
+            );
+            assert.strictEqual(
+                new BeatSnap(new Fraction(1, 13)).nearestCommonSnapIndex(),
+                2,
+            );
+        });
+    });
+
+    describe("#next", () => {
+        it("jumps to next common snapping if not common", () => {
+            const beatSnap = new BeatSnap(new Fraction(1, 5));
+            beatSnap.next();
+            assert.deepStrictEqual(beatSnap.current, new Fraction(1, 8));
+        });
+
+        it("jumps to next common snapping if common", () => {
+            const beatSnap = new BeatSnap(new Fraction(1, 4));
+            beatSnap.next();
+            assert.deepStrictEqual(beatSnap.current, new Fraction(1, 8));
+        });
+
+        it("doesn't go out of range", () => {
+            const snap = commonBeatSnaps[commonBeatSnaps.length - 1];
+            const beatSnap = new BeatSnap(snap);
+            beatSnap.next();
+            assert.deepStrictEqual(beatSnap.current, snap);
+        });
+    });
+
+    describe("#prev", () => {
+        it("jumps to prev common snapping if not common", () => {
+            const beatSnap = new BeatSnap(new Fraction(1, 5));
+            beatSnap.prev();
+            assert.deepStrictEqual(beatSnap.current, new Fraction(1, 4));
+        });
+
+        it("jumps to prev common snapping if common", () => {
+            const beatSnap = new BeatSnap(new Fraction(1, 8));
+            beatSnap.prev();
+            assert.deepStrictEqual(beatSnap.current, new Fraction(1, 4));
+        });
+
+        it("doesn't go out of range", () => {
+            const snap = commonBeatSnaps[0];
+            const beatSnap = new BeatSnap(snap);
+            beatSnap.prev();
+            assert.deepStrictEqual(beatSnap.current, snap);
         });
     });
 
--- client/src/notefield/beatsnap.ts
+++ client/src/notefield/beatsnap.ts
@@ -31,6 +31,64 @@ export class BeatSnap {
         this._current = val;
     }
 
+    /**
+     * Returns true if this snapping matches one of the snaps defined in the
+     * commonBeatSnaps list.
+     */
+    isCommonSnap(): boolean {
+        return (
+            commonBeatSnaps.findIndex((snap) => snap.equals(this.current)) !==
+            -1
+        );
+    }
+
+    /**
+     * Returns the index from the commonBeatSnaps list that closest matches this beat
+     * snapping.
+     */
+    nearestCommonSnapIndex(): number {
+        let minDifference = 999999999;
+        let nearestIndex = 0;
+
+        for (let i = 0; i < commonBeatSnaps.length; i++) {
+            const diff = commonBeatSnaps[i].sub(this.current).abs().valueOf();
+
+            if (diff < minDifference) {
+                minDifference = diff;
+                nearestIndex = i;
+            }
+        }
+
+        return nearestIndex;
+    }
+
+    /**
+     * Makes the beat snapping finer by moving to the next common beat snapping.
+     */
+    next() {
+        let index = this.nearestCommonSnapIndex();
+
+        if (commonBeatSnaps[index].compare(this.current) !== -1) {
+            index++;
+        }
+
+        this.current =
+            commonBeatSnaps[Math.min(index, commonBeatSnaps.length - 1)];
+    }
+
+    /**
+     * Makes the beat snapping coarser by moving to the previous common beat snapping.
+     */
+    prev() {
+        let index = this.nearestCommonSnapIndex();
+
+        if (commonBeatSnaps[index].compare(this.current) !== 1) {
+            index--;
+        }
+
+        this.current = commonBeatSnaps[Math.max(index, 0)];
+    }
+
     toBeat(): Beat {
         return new Beat(this._current.mul(4).valueOf());
     }
