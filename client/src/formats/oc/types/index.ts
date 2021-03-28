edc4c2ca1655c0453fad3cee89e55098b69e72d5
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-03-28 14:35:35 -0400

    Update oc/types/index.ts

--- client/src/formats/oc/types/index.ts
+++ client/src/formats/oc/types/index.ts
@@ -1,3 +1,5 @@
 export { Beat, BeatConverter } from "./beat";
 export { BPM, BPMConverter } from "./bpm";
 export { Chart, ChartConverter } from "./chart";
+export { Hold, HoldConverter } from "./hold";
+export { Tap, TapConverter } from "./tap";
