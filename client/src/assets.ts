a82791e219f766bc49e752b5cdb6caa2753ad706
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-03-30 13:34:13 -0400

    Add audio file for metronome tick

--- client/src/assets.ts
+++ client/src/assets.ts
@@ -1,5 +1,6 @@
 import banner from "../../branding/banner_small.png";
+import sndTick from "../audio/tick.ogg";
 import discord from "../img/discord.png";
 import github from "../img/github.png";
 
-export { banner, discord, github };
+export { banner, discord, github, sndTick };
--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,3 +1,8 @@
+declare module "*.ogg" {
+    const value: string;
+    export default value;
+}
+
 declare module "*.png" {
     const value: string;
     export default value;
