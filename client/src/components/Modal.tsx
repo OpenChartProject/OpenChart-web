adab874efbdd589d30b8d3f48216b55f2d8528a2
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-03-30 17:03:14 -0400

    Extract Modal into its own component

---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+
+export interface Props {
+    className?: string;
+    onClose(): void;
+}
+
+export const Modal = (props: React.PropsWithChildren<Props>) => {
+    const onKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            e.preventDefault();
+            props.onClose();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", onKeyDown);
+        return () => window.removeEventListener("keydown", onKeyDown);
+    }, []);
+
+    return (
+        <div>
+            <div className="dimmer" onClick={props.onClose}></div>
+            <div className={`modal ${props.className || ""}`}>
+                {props.children}
+            </div>
+        </div>
+    );
+};
--- client/src/components/WelcomeModal.tsx
+++ client/src/components/WelcomeModal.tsx
@@ -1,42 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { banner } from "../assets";
+
+import { Modal } from "./Modal";
 
 export interface Props {
     onClose(): void;
 }
 
 export const WelcomeModal = (props: Props) => {
-    const onKeyDown = (e: KeyboardEvent) => {
-        e.preventDefault();
-
-        if (e.key === "Escape" || e.key === "Enter") {
-            props.onClose();
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("keydown", onKeyDown);
-        return () => window.removeEventListener("keydown", onKeyDown);
-    }, []);
-
     return (
-        <div>
-            <div className="dimmer" onClick={props.onClose}></div>
-            <div className="modal welcome-modal">
-                <img className="oc-banner" src={banner} />
+        <Modal onClose={props.onClose} className="welcome-modal">
+            <img className="oc-banner" src={banner} />
 
-                <p>OpenChart is an open source rhythm game chart/map editor.</p>
-                <p>
-                    <strong>
-                        This project is currently in active development -- it is unfinished.
-                    </strong>
-                </p>
-                <p>See the links in the sidebar to learn more.</p>
-                <button className="close-btn" onClick={props.onClose} autoFocus>
-                    OK
-                </button>
-            </div>
-        </div>
+            <p>OpenChart is an open source rhythm game chart/map editor.</p>
+            <p>
+                <strong>
+                    This project is currently in active development -- it is unfinished.
+                </strong>
+            </p>
+            <p>See the links in the sidebar to learn more.</p>
+            <button className="close-btn" onClick={props.onClose} autoFocus>
+                OK
+            </button>
+        </Modal>
     );
 };
