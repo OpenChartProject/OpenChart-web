21e05a03b870e75c343120fd08c2e7cba5eb3331
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-03-30 21:14:45 -0400

    More comments

--- client/src/formats/oc/serializer.ts
+++ client/src/formats/oc/serializer.ts
@@ -2,6 +2,9 @@ import { ISerializer } from "../serializer";
 
 import { FileData } from "./fileData";
 
+/**
+ * Serializer for reading and writing .oc project files.
+ */
 export class Serializer implements ISerializer<FileData> {
     read(contents: string): FileData {
         return JSON.parse(contents);
--- client/src/formats/ocz/serializer.ts
+++ client/src/formats/ocz/serializer.ts
@@ -5,6 +5,13 @@ import { OCSerializer } from "../oc";
 import { FileData } from "../oc/fileData";
 import { ISerializer } from "../serializer";
 
+/**
+ * Serializer for reading and writing compressed .ocz files.
+ *
+ * .ocz files are compressed using the DEFLATE algorithm and then encoded in base64 so
+ * we don't need to worry about reading/writing binary. Even with the extra space needed
+ * for the base64 encoding, the compressed size is still much smaller.
+ */
 export class Serializer implements ISerializer<FileData> {
     read(contents: string): FileData {
         const decompressed = inflate(toByteArray(contents), { to: "string" });
