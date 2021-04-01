9c44da23b049c7d968ef0cf648526e6747a40832
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-01 15:16:45 -0400

    Add NoteFieldStore

---
+++ client/src/store/noteField.ts
@@ -0,0 +1,104 @@
+import assert from "assert";
+import Fraction from "fraction.js";
+import { makeAutoObservable } from "mobx";
+import { BeatTime, Chart, Time } from "../charting";
+import { NoteFieldState } from "../notefield/config";
+import { AutoScroller } from "./autoScroller";
+import { Music } from "./music";
+
+export const zoom = {
+    min: new Fraction(256, 6561),
+    max: new Fraction(6561, 256),
+};
+
+export class NoteFieldStore {
+    chart: Chart;
+    state: NoteFieldState;
+    canvas?: HTMLCanvasElement;
+
+    autoScroller: AutoScroller;
+    music: Music;
+
+    constructor(state: NoteFieldState, chart: Chart, canvas?: HTMLCanvasElement) {
+        makeAutoObservable(this, {
+            canvas: false,
+            autoScroller: false,
+            music: false,
+        });
+        this.chart = chart;
+        this.state = state;
+        this.canvas = canvas;
+
+        this.autoScroller = new AutoScroller(this);
+        this.music = new Music();
+    }
+
+    /**
+     * Resets the scroll and zoom to the default.
+     */
+     resetView() {
+        this.setScroll({ time: Time.Zero });
+        this.setZoom(new Fraction(1));
+    }
+
+    /**
+     * Updates the render height of the notefield.
+     */
+     setHeight(height: number) {
+        // Only update the height if we need to, since the canvas is cleared each time.
+        if (height !== this.state.height) {
+            if (this.canvas) {
+                this.canvas.height = height;
+            }
+
+            this.state.height = height;
+        }
+    }
+
+    setPlaying(isPlaying: boolean) {
+        if (isPlaying !== this.state.isPlaying) {
+            this.state.isPlaying = isPlaying;
+
+            if (isPlaying) {
+                this.autoScroller.start();
+                this.music.playAt(this.state.scroll.time.value);
+            } else {
+                this.music.pause();
+            }
+        }
+    }
+
+    /**
+     * Sets the scroll position to a specific beat/time.
+     */
+    setScroll({ beat, time }: Partial<BeatTime>) {
+        if (beat !== undefined) {
+            time = this.chart.bpms.timeAt(beat);
+            this.state.scroll = { beat, time };
+        } else if (time !== undefined) {
+            beat = this.chart.bpms.beatAt(time);
+            this.state.scroll = { beat, time };
+        } else {
+            throw Error("beat or time must be set");
+        }
+    }
+
+    /**
+     * Sets the notefield zoom.
+     */
+    setZoom(to: Fraction) {
+        assert(to.compare(0) === 1, "zoom must be greater than zero");
+
+        let val = to;
+
+        if (val.compare(zoom.max) === 1) {
+            val = zoom.max;
+        } else if (val.compare(zoom.min) === -1) {
+            val = zoom.min;
+        }
+
+        if (!this.state.zoom.equals(val)) {
+            this.state.zoom = val;
+        }
+    }
+}
