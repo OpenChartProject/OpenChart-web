087c36147b7ca95447518e105e9ed993818668a9
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-02 11:56:29 -0400

    Make the panel container a hideable drawer

--- client/src/components/panels/PanelContainer.tsx
+++ client/src/components/panels/PanelContainer.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const PanelContainer = (props: React.PropsWithChildren<{}>) => {
-    return <div className="panel-container">{props.children}</div>;
+export interface Props {
+    initClosed?: boolean;
+}
+
+export const PanelContainer = (props: React.PropsWithChildren<Props>) => {
+    const [closed, setClosed] = useState(props.initClosed === true);
+
+    const toggle = () => {
+        setClosed(!closed);
+    };
+
+    return (
+        <div className={`panel-container-drawer ${closed ? "closed" : ""}`}>
+            <div className="panel-drawer-btns" title="Toggle side panel">
+                {!closed && (
+                    <div className="panel-drawer-btn" onClick={toggle}>
+                        &gt;
+                    </div>
+                )}
+                {closed && (
+                    <div className="panel-drawer-btn" onClick={toggle}>
+                        &lt;
+                    </div>
+                )}
+            </div>
+
+            <div className="panel-container">{props.children}</div>
+        </div>
+    );
 };
