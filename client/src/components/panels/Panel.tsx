fe2fa102b5d23be0da2260e86152380e5dd0aee8
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-04-02 15:39:45 -0400

    Make panels collapsible

--- client/src/components/panels/Panel.tsx
+++ client/src/components/panels/Panel.tsx
@@ -1,15 +1,30 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { CSSProperties, useState } from "react";
 
 export interface Props {
+    collapsed?: boolean;
     title: string;
 }
 
 export const Panel = observer((props: React.PropsWithChildren<Props>) => {
+    const [collapsed, setCollapsed] = useState(props.collapsed ?? false);
+    const style: CSSProperties = {};
+
+    if (collapsed) {
+        style.display = "none";
+    }
+
     return (
         <div className="panel">
-            <div className="panel-title">{props.title}</div>
-            <div className="panel-content">{props.children}</div>
+            <div
+                className={`panel-title ${collapsed ? "collapsed" : ""}`}
+                onClick={() => setCollapsed(!collapsed)}
+            >
+                {props.title}
+            </div>
+            <div className="panel-content" style={style}>
+                {props.children}
+            </div>
         </div>
     );
 });
