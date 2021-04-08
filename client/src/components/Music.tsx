4a68e02dabbcbf0dfe6966971fe728437bfb39fb
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-04-08 12:40:59 -0400

    Fill out Music component

--- client/src/components/Music.tsx
+++ client/src/components/Music.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import { RootStore } from "../store";
 
@@ -8,5 +8,48 @@ export interface Props {
 }
 
 export const Music = observer(({ store }: Props) => {
-    return <audio></audio>;
+    const ref = useRef<HTMLAudioElement>(null);
+    const { volume } = store.ui.data.music;
+
+    const onPlay = () => {
+        const el = ref.current!;
+        el.play();
+    };
+
+    const onPause = () => {
+        const el = ref.current!;
+        el.pause();
+    };
+
+    const onSeek = (time: number) => {
+        const el = ref.current!;
+        el.currentTime = time;
+    };
+
+    useEffect(() => {
+        const music = store.ui.emitters.music;
+
+        music
+            .on("play", onPlay)
+            .on("pause", onPause)
+            .on("seek", onSeek);
+
+        return () => {
+            music
+                .off("play", onPlay)
+                .off("pause", onPause)
+                .off("seek", onSeek);
+            return;
+        }
+    });
+
+    useEffect(() => {
+        if (!ref.current) {
+            return;
+        }
+
+        ref.current.volume = volume;
+    }, [ref, volume]);
+
+    return <audio ref={ref} src={store.ui.data.music.src}></audio>;
 });
