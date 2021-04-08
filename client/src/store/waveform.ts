b9caf21eca403150a8dba0bf1e5c82e4d406e520
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-04-07 20:20:16 -0400

    Move waveform store data into an interface

--- client/src/store/waveform.ts
+++ client/src/store/waveform.ts
@@ -4,23 +4,30 @@ import WaveformData from "waveform-data";
 
 import { RootStore } from "./store";
 
-export class WaveformStore {
+export interface WaveformStoreData {
     audioData?: ArrayBuffer;
     el?: SVGElement;
     waveform?: WaveformData;
+}
+
+export class WaveformStore {
+    data: WaveformStoreData;
     root: RootStore;
 
     constructor(store: RootStore) {
         this.root = store;
+        this.data = {};
     }
 
     /**
      * Generates a SVG of the waveform with the given width and height and returns it.
      */
     generateSVG(width: number, height: number) {
-        assert(this.el, "an SVG element must be set before generating the waveform");
+        const { el } = this.data;
 
-        const waveform = this.waveform as WaveformData;
+        assert(el, "an SVG element must be set before generating the waveform");
+
+        const waveform = this.data.waveform as WaveformData;
         waveform.resample({ width });
 
         const channel = waveform.channel(0);
@@ -39,7 +46,8 @@ export class WaveformStore {
             .y0((d, i) => y(min[i]))
             .y1((d, i) => y(d as any));
 
-        const svg = d3.select(this.el);
+        const svg = d3.select(el);
+        svg.selectChildren().remove();
 
         svg.style("width", width + "px")
             .style("height", height + "px")
@@ -87,19 +95,19 @@ export class WaveformStore {
      * Sets the audio data to use for the waveform.
      */
     setAudioData(data: ArrayBuffer): Promise<void> {
-        if (data === this.audioData) {
+        if (data === this.data.audioData) {
             return new Promise<void>((resolve) => resolve());
         }
 
         return new Promise<void>((resolve) => {
             this.generateWaveformData(data).then((waveformData) => {
-                this.waveform = waveformData;
+                this.data.waveform = waveformData;
                 resolve();
             });
         });
     }
 
     setElement(el: SVGElement) {
-        this.el = el;
+        this.data.el = el;
     }
 }
