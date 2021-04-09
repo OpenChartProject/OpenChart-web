fe400261a278df7218e1d168b9d54e742eb06e45
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-04-08 20:14:45 -0400

    Simplify TimePicker

--- client/src/components/TimePicker.tsx
+++ client/src/components/TimePicker.tsx
@@ -8,33 +8,22 @@ export interface Props {
 }
 
 export const TimePicker = observer(({ store }: Props) => {
-    const ref = useRef<HTMLDivElement>(null);
     const [y, setY] = useState(0);
 
-    useEffect(() => {
-        if (!ref.current) {
-            return;
+    const onClick = () => {
+        const { timePicker } = store.ui.tools;
+
+        if (timePicker.onPick) {
+            timePicker.onPick();
         }
-
-        ref.current.addEventListener("mousemove", (e) => {
-            setY(e.clientY);
-        });
-
-        ref.current.addEventListener("click", (e) => {
-            const { timePicker } = store.ui.tools;
-
-            if (timePicker.onPick) {
-                timePicker.onPick();
-            }
-        });
-    }, [ref]);
+    }
 
     const style: CSSProperties = {
         top: (y - 1) + "px",
     };
 
     return (
-        <div className="time-selector-container" ref={ref}>
+        <div className="time-selector-container" onClick={onClick} onMouseMove={(e) => setY(e.clientY)}>
             <div className="time-selector-bar" style={style}></div>
         </div>
     );
