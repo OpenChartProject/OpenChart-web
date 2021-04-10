f85fca0565dfb79431e535564cd10166a5e87f1a
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-10 10:57:27 -0400

    Test BPM

---
+++ client/src/charting/bpm.spec.ts
@@ -0,0 +1,25 @@
+import assert from "assert";
+
+import { BPM } from "./bpm";
+
+describe("BPM", () => {
+    describe("set value", () => {
+        it("throws if bpm is <= 0", () => {
+            assert.throws(() => new BPM(0, 0));
+        });
+    });
+
+    describe("#beatsPerSecond", () => {
+        it("returns the expected value", () => {
+            const bpm = new BPM(0, 120);
+            assert.strictEqual(bpm.beatsPerSecond(), 2);
+        });
+    });
+
+    describe("#secondsPerBeat", () => {
+        it("returns the expected value", () => {
+            const bpm = new BPM(0, 120);
+            assert.strictEqual(bpm.secondsPerBeat(), 0.5);
+        });
+    });
+});
