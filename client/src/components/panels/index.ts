6282490b13c0efee05a95a4e8e3f1eb6baaa91c3
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-15 23:42:33 -0400

    Rename file

--- client/src/components/panels/index.ts
+++ client/src/components/panels/index.ts
@@ -1,7 +1,7 @@
 export { AudioPanel } from "./AudioPanel";
 export { AudioOffsetPanel } from "./AudioOffsetPanel";
 export { BeatTimePanel } from "./BeatTimePanel";
-export { BPMPanel } from "./BPM";
+export { BPMPanel } from "./BPMPanel";
 export { NotefieldPanel } from "./NotefieldPanel";
 export { Panel } from "./Panel";
 export { PanelContainer } from "./PanelContainer";
