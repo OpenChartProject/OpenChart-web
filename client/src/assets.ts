bb149983238f9a4606d7d3f99e0c37e519e9566f
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-19 20:17:47 -0400

    Remove tick.ogg

--- client/src/assets.ts
+++ client/src/assets.ts
@@ -1,6 +1,5 @@
 import banner from "../../branding/banner_small.png";
-import sndTick from "../audio/tick.ogg";
 import discord from "../img/discord.png";
 import github from "../img/github.png";
 
-export { banner, discord, github, sndTick };
+export { banner, discord, github };
