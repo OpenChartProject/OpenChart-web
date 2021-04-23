9dc8b0014a8f2ff17343192f176fad5deb69f21d
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-23 18:47:31 -0400

    Start parsing SM fields, added some tests

--- client/src/formats/sm/serializer.spec.ts
+++ client/src/formats/sm/serializer.spec.ts
@@ -1,6 +1,90 @@
+import assert from "assert";
+
+import { BPM, FileData } from "./fileData";
+import { Serializer } from "./serializer";
+
 describe("sm/serializer", () => {
     describe("#read", () => {
-        it("reads a valid file");
+        it("sets default values for missing fields", () => {
+            const data = new Serializer().read("");
+            const expected: FileData = {
+                files: {
+                    background: "",
+                    banner: "",
+                    cdTitle: "",
+                },
+                song: {
+                    artist: "",
+                    bpms: [],
+                    offset: 0,
+                    previewLength: 0,
+                    previewStart: 0,
+                    subtitle: "",
+                    title: "",
+                },
+            };
+
+            assert.deepStrictEqual(data, expected);
+        });
+
+        it("reads the file paths", () => {
+            const input = `
+            #BACKGROUND:bg;
+            #BANNER:banner;
+            #CDTITLE:cdtitle;
+            `;
+            const data = new Serializer().read(input);
+
+            assert.strictEqual(data.files.background, "bg");
+            assert.strictEqual(data.files.banner, "banner");
+            assert.strictEqual(data.files.cdTitle, "cdtitle");
+        });
+
+        it("reads the song data", () => {
+            const input = `
+            #ARTIST:artist;
+            #SUBTITLE:subtitle;
+            #TITLE:title;
+            #OFFSET:1.23;
+            #SAMPLESTART:4.56;
+            #SAMPLELENGTH:7.89;
+            `;
+            const data = new Serializer().read(input);
+
+            assert.strictEqual(data.song.artist, "artist");
+            assert.strictEqual(data.song.subtitle, "subtitle");
+            assert.strictEqual(data.song.title, "title");
+            assert.strictEqual(data.song.offset, 1.23);
+            assert.strictEqual(data.song.previewStart, 4.56);
+            assert.strictEqual(data.song.previewLength, 7.89);
+        });
+
+        it("reads the bpms when there's one bpm change", () => {
+            const input = "#BPMS:0.000=123.456;";
+            const data = new Serializer().read(input);
+            const expected: BPM[] = [{ beat: 0, value: 123.456 }];
+
+            assert.deepStrictEqual(data.song.bpms, expected);
+        });
+
+        it("reads the bpms when there's multiple bpm changes", () => {
+            const input = "#BPMS:0.000=123.456,5.432=987.654;";
+            const data = new Serializer().read(input);
+            const expected: BPM[] = [
+                { beat: 0, value: 123.456 },
+                { beat: 5.432, value: 987.654 },
+            ];
+
+            assert.deepStrictEqual(data.song.bpms, expected);
+        });
+
+        it("reads the bpms when the values don't include decimals", () => {
+            const input = "#BPMS:0=120;";
+            const data = new Serializer().read(input);
+            const expected: BPM[] = [{ beat: 0, value: 120 }];
+
+            assert.deepStrictEqual(data.song.bpms, expected);
+        });
     });
 
     describe("#writes", () => {
--- client/src/formats/sm/serializer.ts
+++ client/src/formats/sm/serializer.ts
@@ -1,10 +1,76 @@
+import _ from "lodash";
+
 import { ISerializer } from "../serializer";
 
+import { readFields } from "./fieldReader";
+import { Fields } from "./fields";
 import { FileData } from "./fileData";
 
 export class Serializer implements ISerializer<FileData> {
     read(contents: string): FileData {
-        throw Error;
+        const data: FileData = {
+            files: {
+                background: "",
+                banner: "",
+                cdTitle: "",
+            },
+            song: {
+                artist: "",
+                bpms: [],
+                offset: 0,
+                previewLength: 0,
+                previewStart: 0,
+                subtitle: "",
+                title: "",
+            },
+        };
+
+        for (const field of readFields(contents)) {
+            const { name, value } = field;
+
+            switch (name) {
+                case Fields.artist:
+                    data.song.artist = value;
+                    break;
+
+                case Fields.background:
+                    data.files.background = value;
+                    break;
+
+                case Fields.banner:
+                    data.files.banner = value;
+                    break;
+
+                case Fields.cdTitle:
+                    data.files.cdTitle = value;
+                    break;
+
+                case Fields.subtitle:
+                    data.song.subtitle = value;
+                    break;
+
+                case Fields.title:
+                    data.song.title = value;
+                    break;
+
+                case Fields.offset:
+                    data.song.offset = _.toNumber(value);
+                    break;
+
+                case Fields.sampleLength:
+                    data.song.previewLength = _.toNumber(value);
+                    break;
+
+                case Fields.sampleStart:
+                    data.song.previewStart = _.toNumber(value);
+                    break;
+
+                default:
+                // skip
+            }
+        }
+
+        return data;
     }
 
     write(data: FileData): string {
