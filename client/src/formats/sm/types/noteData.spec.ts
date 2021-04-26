8e9da7518716c7c9b07e5d442a57f8ffa496920a
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-04-26 11:38:19 -0400

    Start implementing NoteDataConverter

--- client/src/formats/sm/types/noteData.spec.ts
+++ client/src/formats/sm/types/noteData.spec.ts
@@ -1,4 +1,5 @@
 import assert from "assert";
+import { Tap } from "../../../charting/objects";
 
 import { NoteDataConverter } from "./noteData";
 
@@ -8,6 +9,20 @@ describe("sm/types", () => {
             it("returns an array with the same length as the key count", () => {
                 assert.strictEqual(new NoteDataConverter(4).toNative([]).length, 4);
                 assert.strictEqual(new NoteDataConverter(7).toNative([]).length, 7);
+            });
+
+            it("converts tap notes", () => {
+                const input = ["1000", "0100", "0010", "0001"].join("");
+                const native = new NoteDataConverter(4).toNative([input]);
+
+                const expected = [
+                    [new Tap(0, 0)],
+                    [new Tap(0.25, 1)],
+                    [new Tap(0.5, 2)],
+                    [new Tap(0.75, 3)],
+                ];
+
+                assert.deepStrictEqual(native, expected);
             });
         });
     });
--- client/src/formats/sm/types/noteData.ts
+++ client/src/formats/sm/types/noteData.ts
@@ -1,8 +1,23 @@
+import Fraction from "fraction.js";
+import { Beat } from "../../../charting";
 import { KeyObjects } from "../../../charting/chart";
+import { Tap } from "../../../charting/objects";
 import { TypeConverter } from "../../converter";
 
 export type Measure = string;
 export type NoteData = Measure[];
+
+export enum NoteType {
+    empty = "0",
+    tap = "1",
+    holdHead = "2",
+    holdRollTail = "3",
+    rollHead = "4",
+    mine = "M",
+    keySound = "K",
+    lift = "L",
+    fake = "F",
+}
 
 /**
  * Converts the SM note data into note data usable by the editor.
@@ -12,6 +27,8 @@ export type NoteData = Measure[];
  * whereas the editor organizes the note data into columns.
  */
 export class NoteDataConverter implements TypeConverter<KeyObjects[], NoteData> {
+    private holds!: (number | null)[];
+    private keys!: KeyObjects[];
     private readonly keyCount: number;
 
     constructor(keyCount?: number) {
@@ -19,22 +36,52 @@ export class NoteDataConverter implements TypeConverter<KeyObjects[], NoteData> 
     }
 
     toNative(data: NoteData): KeyObjects[] {
-        const keys: KeyObjects[] = [];
+        this.holds = [];
+        this.keys = [];
 
         for (let i = 0; i < this.keyCount; i++) {
-            keys.push([]);
+            this.holds.push(null);
+            this.keys.push([]);
         }
 
+        let measureBeat = -1;
+
         for (const measure of data) {
+            measureBeat++;
+
             if (!measure) {
                 continue;
             }
+
+            this.convertMeasureToNative(measure, measureBeat);
         }
 
-        return keys;
+        return this.keys;
     }
 
     fromNative(data: KeyObjects[]): NoteData {
         throw new Error("Method not implemented.");
+    }
+
+    private convertMeasureToNative(measure: Measure, measureBeat: number) {
+        const rows = measure.length / this.keyCount;
+        let beat = new Fraction(measureBeat);
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < this.keyCount; j++) {
+                const ch = measure[i * this.keyCount + j];
+
+                switch (ch as NoteType) {
+                    case NoteType.tap:
+                        this.keys[j].push(new Tap(new Beat(beat), j));
+                        break;
+
+                    default:
+                    // skip
+                }
+            }
+
+            beat = beat.add(1, rows);
+        }
     }
 }
