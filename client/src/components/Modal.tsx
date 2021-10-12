34249801419f347362184201f283fbc84235e882
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-10-12 00:04:09 -0400

    Add closing logic to Modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,9 +1,12 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
+import { store } from "..";
+import { RootStore } from "../store";
 
 export interface Props {
     className?: string;
-    onClose(): void;
+    store: RootStore;
+    onClose?(): void;
 }
 
 /**
@@ -11,10 +14,18 @@ export interface Props {
  * a modal with the component's children rendered as the contents.
  */
 export const Modal = observer((props: React.PropsWithChildren<Props>) => {
+    const closeModal = () => {
+        if (props.onClose) {
+            props.onClose();
+        }
+
+        store.ui.hideModal();
+    }
+
     const onKeyDown = (e: KeyboardEvent) => {
         if (e.key === "Escape") {
             e.preventDefault();
-            props.onClose();
+            closeModal();
         }
     };
 
@@ -25,7 +36,7 @@ export const Modal = observer((props: React.PropsWithChildren<Props>) => {
 
     return (
         <div>
-            <div className="dimmer" onClick={props.onClose}></div>
+            <div className="dimmer" onClick={closeModal}></div>
             <div className={`modal ${props.className || ""}`}>{props.children}</div>
         </div>
     );
